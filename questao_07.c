f46c51b3758c8a344820f4ab2f47abd2e7a5d21f
Wagner Negrao <wagnerfelidre@gmail.com>
2018-07-24 19:32:49 -0300

    Parte 1 e 2 feitas, falta parte 3 e 4

---
+++ questao_07.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 2
+
+typedef struct{
+	char nome[100];
+	char setor; //Apenas uma letra
+	int quantidade;
+	float preco
+}Supermercado;
+
+Supermercado estoque[TAM];
+void CadastraProduto();
+void ListarProdutos();
+
+
+int main(){
+	int opcao = 1;
+
+	while(opcao != 0){
+		printf("\nDigite sua opcao: \n");
+		printf("\n[1] - Cadastrar Produto");
+		printf("\n[2] - Listar Produtos");
+		printf("\n[3] - Produtos do Setor");
+		printf("\n[4] - Total investido");
+		printf("\n[0] - Sair\n -> ");
+		scanf("%i", &opcao);
+		getchar();
+		
+		switch(opcao){
+			case 1:
+				CadastraProduto();
+				break;
+			case 2:
+				ListarProdutos();
+				break;
+		}
+	}
+	
+
+	return 0;
+}
+
+void CadastraProduto(){
+	int i;
+	printf("\n\nCADASTRO DE PRODUTOS\n\n");
+	for(i = 0; i < TAM; i++){
+		printf("\nNome: ");
+		gets(estoque[i].nome);
+		
+		printf("\nSetor: ");
+		scanf("%c", &estoque[i].setor);
+		getchar();
+
+		printf("\nQuantidade: ");
+		scanf("%i", &estoque[i].quantidade);
+		getchar();
+
+		printf("\nPreco: ");
+		scanf("%f", &estoque[i].preco);
+		getchar();
+
+		printf("\n\n======================\n\n");
+	}
+}
+
+void ListarProdutos(){
+	int i;
+	for(i = 0; i < TAM; i++){
+		printf("\nNome: %s", estoque[i].nome);
+		printf("\nSetor: %c", estoque[i].setor);
+		printf("\nQuantidade: %i", estoque[i].quantidade);
+		printf("\nPreco: %.2f\n", estoque[i].preco);
+
+		printf("\n\n======================\n\n");
+	}
+}
